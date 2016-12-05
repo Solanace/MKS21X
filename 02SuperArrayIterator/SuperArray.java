c311d0d977395de1c0003eb21ee928a014b71a99
Penn Wu <penn.z.wu@gmail.com>
2016-12-04 22:46:56 -0500

    Added SuperArrayIterator into SuperArray

--- 02SuperArrayIterator/SuperArray.java
+++ 02SuperArrayIterator/SuperArray.java
@@ -1,4 +1,5 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 public class SuperArray implements Iterable<String>{
     private String[] data;
     private int size;
@@ -191,5 +192,33 @@ public class SuperArray implements Iterable<String>{
 
     public Iterator<String> iterator() {
 	return new SuperArrayIterator(this);
+    }
+}
+
+class SuperArrayIterator implements Iterator<String> {
+    private SuperArray array;
+    private int element;
+
+    public SuperArrayIterator(SuperArray array) {
+	this.array = array;
+	element = 0;
+    }
+
+    public boolean hasNext() {
+	return element < array.size();
+    }
+
+    public String next() {
+	if (hasNext()){
+	    String s = array.get(element);
+	    element += 1;
+	    return s;
+	}
+
+	else throw new NoSuchElementException();
+    }
+    
+    public void remove() {
+	throw new UnsupportedOperationException();
     }
 }
