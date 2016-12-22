52582193794feb573adbf6f7d15b829af228c93c
Penn Wu <penn.z.wu@gmail.com>
2016-12-21 22:12:45 -0500

    Created a working Window

--- Temperature/Temperature.java
+++ Temperature/Temperature.java
@@ -1,15 +1,15 @@
 public class Temperature {
-    public static double CtoF(double t) {
+    public static double toF(double t) {
 	return t * 9/5 + 32;
     }
 
-    public static double FtoC(double t) {
+    public static double toC(double t) {
 	return (t - 32) * 5/9;
     }
 
     public static void main(String[] args) {
-	System.out.println(CtoF(-40));
-	System.out.println(CtoF(100));
-	System.out.println(FtoC(32));
+	System.out.println(toF(-40));
+	System.out.println(toF(100));
+	System.out.println(toC(32));
     }
 }
--- Temperature/Window.java
+++ Temperature/Window.java
@@ -1,12 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
-public class Window extends JFrame {
-    private Container frame;
-
-    private JButton toC;
-    private JButton toF;
+import java.awt.event.*;
+public class Window extends JFrame implements ActionListener{
+    private Container pane;
     private JTextField displayTemp;
-
+    
     public Window() {
 	this.setTitle("Temperature Converter");
 	this.setSize(600,400);
@@ -15,5 +13,39 @@ public class Window extends JFrame {
 	
 	pane = this.getContentPane();
 	pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
-	toC = new JButton("to Celsius");
-	toF = new JButton("to Fahrenheit");
+	JButton toC = new JButton("to Celsius");
+	toC.addActionListener(this);
+	toC.setActionCommand("toC");
+	
+	JButton toF = new JButton("to Fahrenheit");
+	toF.addActionListener(this);
+	toF.setActionCommand("toF");
+	
+        displayTemp = new JTextField("100", 12);
+
+	pane.add(toC);
+	pane.add(toF);
+	pane.add(displayTemp);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+	String event = e.getActionCommand();
+	
+	if (event.equals("toC")) {
+	    double c = Double.parseDouble(displayTemp.getText());
+	    c = Temperature.toC(c);
+	    displayTemp.setText("" + c);
+	}
+	
+	if (event.equals("toF")) {
+	    double f = Double.parseDouble(displayTemp.getText());
+	    f = Temperature.toF(f);
+	    displayTemp.setText("" + f);
+	}
+    }
+
+    public static void main(String[] args) {
+	Window g = new Window();
+	g.setVisible(true);
+    }
+}
