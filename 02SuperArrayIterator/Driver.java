d60c3c1a9a37e3e1dda0180daf0759abaefbe3de
Penn Wu <penn.z.wu@gmail.com>
2016-12-22 05:34:57 -0500

    Made Driver actually do SuperArrayIterator tests

--- 02SuperArrayIterator/Driver.java
+++ 02SuperArrayIterator/Driver.java
@@ -1,22 +1,15 @@
 public class Driver{
   public static void main(String[] args) {
-    SuperArray data = new SuperArray();
+    SuperArray data = new SuperArray(0);
+    SuperArrayIterator looper = new SuperArrayIterator(data);
     int i = 0;
-    while(i < 26){
+    /*while (i < 26) {
       data.add(""+(char)('A'+i%26));
       i++;
-    }
+      }*/
 
-    System.out.println(data);
-    System.out.println("Standard loop:");
-
-    for(int n = 0; n < data.size(); n++){
-      System.out.print(data.get(n)+" ");
-    }
-    System.out.println();
-    System.out.println("for-each loop:");
-    for(String s : data){
-      System.out.print(s+" ");
+    while (looper.hasNext()) {
+	System.out.print(looper.next() + "|");
     }
   }
 }
